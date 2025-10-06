ab6006a43d22d3379b75f8476615f96c24f50dbf
NagyIstvanGabor <nagy.istvan.gabor@protonmail.com>
2025-10-06 16:33:45 +0200

    initial commit - setting up the repo for stream-3

---
+++ lessons/lesson_01/main.py
@@ -0,0 +1,44 @@
+print("Hello World!")
+
+# this is a comment / ez egy komment -> SINGLE LINE COMMENT
+
+print("Hello world again!")
+
+"""
+THIS
+IS
+A
+MULTI LINE COMMENT
+"""
+
+'''
+THIS
+IS
+A
+MULTI LINE COMMENT TOO
+'''
+
+#PRIMITIVE DATA TYPES / PRIMITÍV ADATTÍPUSOK
+
+#STRING TÍPUS - KARAKTERLÁNC
+print("apple")
+print("This is an apple")
+print('This is an apple')
+
+print("3")
+print("True")
+
+#INTEGER TÍPUS - EGÉSZ SZÁM
+print(1)
+print(122312312)
+
+#FLOAT (FLOATING POINT NUMBER)
+print(2.14)
+print(54335453.12321313)
+
+#BOOLEAN
+print(True)
+print(False)
+
+#NONE TYPE
+print(None) #az információ hiányát jelöli
---
+++ lessons/lesson_01/virtual_env_practice.py
@@ -0,0 +1,9 @@
+#python standard library
+import random
+
+print(random.randint(1,10))
+
+#pypi
+import fastapi
+
+app = fastapi.FastAPI()

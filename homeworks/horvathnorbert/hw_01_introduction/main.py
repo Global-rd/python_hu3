fa5123f6ae6e36de18cbc62f770896abab1bcb60
HorvathNorbert-dev <horvathnorbi@gmail.com>
2025-10-08 20:04:38 +0200

    Homework 1 done!
    Szia István! Ha minden igaz elkészültem az első házi feladattal. Köszönöm, hogy átnézed!

---
+++ homeworks/horvathnorbert/hw_01_introduction/main.py
@@ -0,0 +1,30 @@
+"""
+"This is 
+my first Python
+program"
+"""
+
+'''
+Ez szintén
+egy több soros
+komment
+'''
+
+#a tanult primitív adattípusok
+
+print("Hello World!") #string - karakterlánc
+print('This works to.') #string - karakterlánc
+print("323") #string - karakterlánc
+print("False") #string - karakterlánc
+
+print(28) #integer - egész szám
+print(20140903) #integer - egész szám
+
+print(3.14) #float - lebegő pontos szám
+print(1234.4567) #float - lebegő pontos szám
+
+print(True) #bolean - logikai típus
+print(False) #bolean - logikai típus
+
+#none - semmi, nincs érték típus
+print(None) #none - semmi, nincs érték típus, az adat vagy információ hiányát jelzi

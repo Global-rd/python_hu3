1499884fc7b73e53d5e71c3a6b292e609b638842
Laszlo Laszlo <laszlo.laszlo@icloud.com>
2025-10-10 18:45:38 +0200

    Initial commit

---
+++ homeworks/laszlolaszlo/hw_01_introduction/main.py
@@ -0,0 +1,18 @@
+"""
+This is my first
+Python program
+"""
+
+# Official document: https://docs.python.org/3/library/stdtypes.html
+
+integer_example = 101
+print(f"This is an integer data type: {integer_example}")
+
+float_example = -101.123
+print(f"This is a float data type: {float_example}")
+
+string_example = "This is a string data type example."
+print(f"This is a string data type: {string_example}")
+
+boolean_example = True
+print(f"This is a boolean data type: {boolean_example}")

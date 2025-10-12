0d83c24e46b828bafd883f6038d7c04ec1aeebeb
tomatamas <tomatae@gmail.com>
2025-10-12 20:26:40 +0200

    Update homework.py

--- homeworks/tomatamas/hw_01_introduction/homework.py
+++ homeworks/tomatamas/hw_01_introduction/homework.py
@@ -6,8 +6,10 @@ int_example = 5
 float_example = 5.23
 string_example = "A wonderful set of characters conjoined as a string!"
 bool_example = False
+none_example = None
 
 print(f"Integer data type example: {int_example}")
 print(f"Float data type example: {float_example}")
 print(f"String data type example: {string_example}")
 print(f"Boolean data type example: {bool_example}")
+print(f"None type example: {none_example}")

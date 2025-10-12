60bd920d7c167595e88ed11d2add1f00d331b5f8
tomatamas <tomatae@gmail.com>
2025-10-12 20:21:26 +0200

    Create homework.py

---
+++ homeworks/tomatamas/hw_01_introduction/homework.py
@@ -0,0 +1,13 @@
+"""
+This is my first python program
+"""
+
+int_example = 5
+float_example = 5.23
+string_example = "A wonderful set of characters conjoined as a string!"
+bool_example = False
+
+print(f"Integer data type example: {int_example}")
+print(f"Float data type example: {float_example}")
+print(f"String data type example: {string_example}")
+print(f"Boolean data type example: {bool_example}")

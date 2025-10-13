4f1c0057f40be97b30e720de81f2ea6c96c925b3
lengyel-sem <laszlo.lengyel@sempergy.hu>
2025-10-13 19:01:56 +0200

    homework1 done

---
+++ homeworks/lengyellaszlo/hw_01_introduction/homework1.py
@@ -0,0 +1,15 @@
+"""
+This is my fi rst Python program
+"""
+
+# Sztring (string)
+print("Hello word!")  # str
+
+# Egész szám (integer)
+print(8)  # int
+
+# Lebegőpontos szám (float)
+print(1.1)  # float
+
+# Logikai érték (boolean)
+print(True)  # bool

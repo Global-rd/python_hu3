695a53fa1145a306692ce1cdb19bb79e97b0dbd4
storkstreetkft-spec <storkstreetkft@gmail.com>
2025-10-13 18:05:01 +0200

    primitives_print

---
+++ homeworks/mezofigabor/hw_01_introduction/homework_1.py
@@ -0,0 +1,9 @@
+i = 20            # int - egész szám
+f = 3.14          # float - lebegőpontos szám
+s = "Hello world!" # str - szöveg
+b = True          # bool - logikai érték
+
+print("int:", i, type(i))
+print("float:", f, type(f))
+print("str:", s, type(s))
+print("bool:", b, type(b))

d0b26a5b56459ead611c92e79405100f398ae6b6
elenasofron <elena.sofronova@gmail.com>
2025-10-14 19:43:15 +0200

    homework1

---
+++ homeworks/elenasofron/homework-1.py
@@ -0,0 +1,31 @@
+print ("""This
+is 
+my
+first 
+Python 
+program""")   
+
+#PRIMITIVE DATA TYPES
+
+#STRING TÍPUS
+print("apple")
+print("This is an apple")
+print('This is an apple')
+
+print("3")
+print("True")
+
+#INTEGER TÍPUS
+print(1)
+print(122312312)
+
+#FLOAT (FLOATING POINT NUMBER)
+print(2.14)
+print(54335453.12321313)
+
+#BOOLEAN
+print(True)
+print(False)
+
+#NONE TYPE
+print(None) 

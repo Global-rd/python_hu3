b8ce0871a5c1edc4717ba79f5f774ce731f3ef4a
Vajandtj <vajandtjudit25@gmail.com>
2025-10-14 19:42:47 +0200

    Add first homework (hw_01_introduction)

---
+++ homeworks/vajandtjudit/hw_01_introduction/homework.py
@@ -0,0 +1,9 @@
+homework.py
+""""
+print("This is my first Python")
+
+print("Hello world!") # str (string)
+print(42)  # int (integer)
+print(3.14)  #float
+print(True)  #bool
+print(None)   #NoneType

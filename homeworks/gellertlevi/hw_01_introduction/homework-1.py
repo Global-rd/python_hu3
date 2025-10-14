0dbdf3b7805821b407c897deb0e8e9d11c5347f1
leventegellert <56770275+leventegellert@users.noreply.github.com>
2025-10-14 14:37:15 +0200

    homework-1 done

---
+++ homeworks/gellertlevi/hw_01_introduction/homework-1.py
@@ -0,0 +1,12 @@
+""" 
+This
+ is my 
+ first 
+ python
+   code
+     """
+
+print (123) # integer - egész szám
+print (2.72) # float - lebegőpontos szám
+print (True) # boolean - logikai típus
+print ("Miért csinálja nekem automatikusan?!") # string - karakterlánc

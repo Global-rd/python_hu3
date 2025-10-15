3cb4b7bef2dc67ed3393167afe6af10698c7a057
elenasofron <elena.sofronova@gmail.com>
2025-10-15 20:28:52 +0200

    hw 01 done

---
+++ homeworks/elenasofron/hw_01_introduction/homework_01.py
@@ -0,0 +1,18 @@
+"""This
+is
+my
+first
+Python
+program"""
+
+#string
+print("text=string") 
+#Integer = number
+print(12) 
+#Float = tört szám
+print(3.14) 
+#Boolean vagyvagy
+print(False) 
+print(True)
+#None type
+print(None) 

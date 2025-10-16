0933064b68e85ecb81c3a956222e75e71b1441ab
mate-byte <kissmate1213@freemail.hu>
2025-10-16 16:11:40 +0200

    Update User_info.py

--- homeworks/kissmate/hw_02_vars_datatypes/User_info.py
+++ homeworks/kissmate/hw_02_vars_datatypes/User_info.py
@@ -13,7 +13,33 @@ user_info = {
 "Jim": "+364005000"
 }
 }
-
+#1.feldata
 language = input("Give me 4 programing language separated with commas and without spaces")
 user_info["skills"] = language
-
+print(language)
+#2 feladat
+user_info["favourite_meals"].sort()
+print(user_info["favourite_meals"])
+#3.feladat
+favourite_meals = user_info.get("favourite_meals")
+print(favourite_meals[-2])
+#4.feladat
+favourite_meals.append("spaghetti")
+print(favourite_meals)
+#5.feladat
+favourite_meals.append(favourite_meals[2])
+favourite_meals.append(favourite_meals[3])
+print(favourite_meals)
+#6.feladat
+favourite_meals = list(set(favourite_meals))
+#7.feladat
+favourite_meals[0], favourite_meals[-1] = favourite_meals[-1], favourite_meals[0]
+print(favourite_meals)
+#8.feladat
+("John", "+36301234567")
+#9.feladat
+user_info["phone_contacts"].pop("Tim")
+print(user_info["phone_contacts"])
+#10.feladat
+user_info["phone_contacts"].append("Jimmy":"+364005001","3670364004")
+print(user_info["phone_contacts"])

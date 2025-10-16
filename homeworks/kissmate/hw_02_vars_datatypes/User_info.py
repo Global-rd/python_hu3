f91e2941cb05af85b7d7b695c4a7e5a49243e98f
mate-byte <kissmate1213@freemail.hu>
2025-10-16 15:26:08 +0200

    Create User_info.py

---
+++ homeworks/kissmate/hw_02_vars_datatypes/User_info.py
@@ -0,0 +1,19 @@
+user_info = {
+"name": "Mike",
+"age": 25,
+"favourite_meals": [
+"pizza",
+"carbonara",
+"sushi"
+],
+"phone_contacts": {
+"Mary": "+36701234567",
+"Tim": "+36207654321",
+"Tim2": "+36304567321",
+"Jim": "+364005000"
+}
+}
+
+language = input("Give me 4 programing language separated with commas and without spaces")
+user_info["skills"] = language
+

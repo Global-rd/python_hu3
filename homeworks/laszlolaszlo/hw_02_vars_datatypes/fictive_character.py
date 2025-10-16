6c7ee4aba89b7c832e34b4b2fc347b40e20a1fea
Laszlo Laszlo <laszlo.laszlo@icloud.com>
2025-10-16 05:57:02 +0200

    fix fictive_character.py with teachers instructions

--- homeworks/laszlolaszlo/hw_02_vars_datatypes/fictive_character.py
+++ homeworks/laszlolaszlo/hw_02_vars_datatypes/fictive_character.py
@@ -8,8 +8,8 @@ character_name = character_name.upper().strip()
 Ez az utóbbi szerintem az olvashatóbb kódot adja.
 """
 
-character_name = str.upper(input("Please enter your name: ").strip())
-character_age = input("Please enter your age: ")
+character_name = input("Please enter your name: ").strip().upper()
+character_age = int(input("Please enter your age: "))
 character_python_experience_in_years = input(
     "Please enter your Python experience in years: "
 )
@@ -17,17 +17,12 @@ is_pro_python_developer = input(
     "Would you like your character to be a professional Python developer? (yes/no): "
 )
 
-character_age = int(character_age)
 character_age_in_days = character_age * 365
 
-developer_msg = (
-    "He/she wants to be a Python developer!"
-    if is_pro_python_developer == "yes"
-    else "He/she does not want to be a Python developer!"
-)
+dev_intention = "wants" if is_pro_python_developer == "yes" else "does not want"
 
 print(
     f"My character is {character_age_in_days} days old. "
     f"His/her name is {character_name} and he/she has {character_python_experience_in_years} year(s) experience. "
-    f"{developer_msg}"
+    f"He/she {dev_intention} to be a Python developer!"
 )

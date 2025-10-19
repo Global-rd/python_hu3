b4da2a8add5cf469562979545d058c689b3dc673
Vajandtj <vajandtjudit25@gmail.com>
2025-10-19 13:08:37 +0200

    javítás

--- homeworks/vajandtjudit/hw_01_introduction/homework.py
+++ homeworks/vajandtjudit/hw_01_introduction/homework.py
@@ -1,17 +1,13 @@
 """
-This
-is
-my
-first
-Python
-program
+# This
+# is
+# my
+# first
+# Python
+# program
 """
-
-
 print("Hello world!") # str (string)
 print(42)  # int (integer)
 print(3.14)  #float
 print(True)  #bool
 print(None)   #NoneType
-
-

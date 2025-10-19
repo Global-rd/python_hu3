58954446889206a146f10de8bf0a11f636cb5a3d
Vajandtj <vajandtjudit25@gmail.com>
2025-10-19 07:12:23 +0200

    Homework.py restored in correct folder

---
+++ homeworks/vajandtjudit/hw_01_introduction/homework.py
@@ -0,0 +1,13 @@
+# This
+# my
+# first
+# Python
+# program
+
+print("Hello Wrold!")
+
+# integer egész számok
+# float tört számok
+# string szöveges adat
+# nonetype üres érték
+# bool logikai érték

9a4d65e58cf5aa8a169cf9c3e4aa42bbfb10a6d3
KZsolt-ux <kzs1964@gmail.com>
2025-10-19 03:14:59 +0200

    Create homework.py

---
+++ homeworks/kovacszsolt/hw_01_introduction/homework.py
@@ -0,0 +1,16 @@
+
+
+"""
+This is
+my first
+Python program
+"""
+
+
+
+print(Szöveges üzenet)  # String/
+print(1234)             # Integer/Egész szám
+print(1.234)            # Floating point/ Lebegőpontos nem egész szám
+print(True)             # Boolean
+print(False)
+print(None)             # None type

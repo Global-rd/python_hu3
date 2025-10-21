49830745d4ca4e71e5d5627bc756702a9955ba05
laci1223 <laszlo.almasi@sempergy.hu>
2025-10-21 21:30:16 +0200

    HW-2 update2

--- homeworks/almasilaszlo/hw_02_vars_datatypes/fictive_character.py
+++ homeworks/almasilaszlo/hw_02_vars_datatypes/fictive_character.py
@@ -19,7 +19,7 @@ fullname=input("Who the hell are you?").upper().strip()
 python_exp=int(input("How long have you been working with pyton (in years)?" ))
 #print(python_exp)
 #javasolt megoldás:
-profi=(input("Wana be a pro pyhton developer? (YES/NO)")).upper()
-answ= "want" if profi=="YES" else "don't want" #ez a jól látom case sensitive, lehet úgy csinálni, hogy ne legyen az?
+prof=(input("Wana be a pro pyhton developer? (YES/NO)")).upper()
+answ= "want" if prof=="YES" else "don't want" #ez a jól látom case sensitive, lehet úgy csinálni, hogy ne legyen az?
 intro= f"My character is {dummy_days} days old. His name is {fullname} and he has {python_exp} years experience. He {answ} to be a pro python developer!! "
 print(intro)

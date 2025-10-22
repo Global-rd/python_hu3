7a2cb383146e04106227f0ed90d9b396cca92e13
kagal <kagal@freemail.hu>
2025-10-22 20:46:27 +0200

    második házi elsőfele

--- homeworks/horvolgyinegalkatalin/hw_02_vars_datatypes/fictive_character.py
+++ homeworks/horvolgyinegalkatalin/hw_02_vars_datatypes/fictive_character.py
@@ -7,25 +7,20 @@ Personnagybetüvel=Person.upper()
 Personnagybetüvelszóköznélkül=Personnagybetüvel.strip()
 
 print("Add meg az életkorod:")
-Életkor=int(input(""))
-Életkornapokban=Életkor*365
-print("Add meg a Pyton tapasztalatodat években:?")
+Életkor=float(input(""))
+Életkornapokban=int(Életkor)*365
+print("Add meg a Pyton tapasztalatodat években:")
 Tudásszint=float(input(""))
 
 """adatok formai ellenőrzése lépésenként, tartalmi ellenőrzést nem tartalmaz:
 azaz nem ellenőrzöm hogy kitöltötte-e a nevet és ha igen betükkel-e, 
-számot írt-e az életkorhoz és 
+egész számot írt-e az életkorhoz és 
 számot írt-e a szakmai tapasztalathoz
 nem nagyobb-e a szakmai tapasztalata az életkoránál
 szükség esetén kidolgozható
 """
-#csak az ellenőrzéshez jó az alábbi 6 print tasítás, élesben kitörülhető
-print(Person)
-print(Personnagybetüvel)
-print(Personnagybetüvelszóköznélkül)
-print(Életkor)
-print(Életkornapokban)
-print (Tudásszint)
 
 # végeredmény tartalmi ellenőrzés nélkül
-print(Personnagybetüvelszóköznélkül,"vagyok",Életkornapokban,"napos vagyok",Tudásszint,"éves Pyton tapasztalattal")
+
+kiirandó=f"My character is {Életkornapokban} old. His/her name is {Personnagybetüvelszóköznélkül} and he/she has {Tudásszint} years experience."
+print(kiirandó)

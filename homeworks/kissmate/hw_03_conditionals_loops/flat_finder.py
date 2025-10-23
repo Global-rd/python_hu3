3f0ed7a7bb599abfdaefea95d691981c55c4eb04
mate-byte <kissmate1213@freemail.hu>
2025-10-23 06:09:40 +0200

    Update flat_finder.py

--- homeworks/kissmate/hw_03_conditionals_loops/flat_finder.py
+++ homeworks/kissmate/hw_03_conditionals_loops/flat_finder.py
@@ -1,4 +1,6 @@
 cities = input("Enter city names separated by commas: ").split(",")
+if any(city.strip() in [ "Chicago"] for city in cities):
+    print(f"You have selected Chicago.")
 budget = float(input("Enter your budget: "))
 if any(city.strip() in ["New York", "San Francisco"] for city in cities) and budget >= 4000:
     print(f"You can afford to live in one of the selected cities.")

bfbf7b0c15ee21d96bbc818e2b7b1d2745a0dfbe
Vajandtj <vajandtjudit25@gmail.com>
2025-10-23 08:58:05 +0200

    remove old homeworks, start hw_03

--- homeworks/vajandtjudit/hw_02_vars_datatypes/fictive_character.py
+++
@@ -1,7 +0,0 @@
-name = input("Add meg a felhasználó nevét:").strip().capitalize()
-age_inpt = input("Add meg a felhasználó életkorát (években):)")
-python_exp = float(input("Add meg a Python tapasztalait:"))
-age = int(age_inpt)
-age_in_days = age * 365
-print(f"A felhasználó neve: {name}")
-print(f"A felhasználó {age_in_days} nap és {python_exp} év tapasztalat")

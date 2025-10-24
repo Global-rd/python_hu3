f60df61515347e9f5655ac0ec49cd58cbad6b45c
juhasz007-maker <juhasz007@gmail.com>
2025-10-24 15:15:09 +0200

    juhaszviktor_hw3_v2_kész

--- homeworks/juhaszviktor/hw_03_conditionals_loops/rock_paper_scissors.py
+++ homeworks/juhaszviktor/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -1,15 +1,15 @@
 while True:
-    max_circle = int(input ("Mennyi kört játszotok (páratlan számot adj meg): "))
-    if max_circle %2 == 1:
+    max_rounds = int(input ("Mennyi kört játszotok (páratlan számot adj meg): "))
+    if max_rounds %2 == 1:
         break
 
 score1 = 0
 score2 = 0
-circle = 1
+round = 1
 
-while circle <=3:
+while round <= max_rounds:
     print("")
-    print(f"{circle}. kör, első játékos: {score1} pont, második játékos {score2} pont.")    
+    print(f"{round}. kör, első játékos: {score1} pont, második játékos {score2} pont.")    
     while True:
         answ1 = input ("Első játékos mit választ (rock/paper/scissors)")
         if answ1 in ["rock", "paper", "scissors"]:
@@ -22,17 +22,16 @@ while circle <=3:
 
     if answ1 == answ2:
         print("döntetlen.")
+        continue
     elif (answ1 == "rock" and answ2 == "paper") or \
         (answ1 == "paper" and answ2 == "scissors") or \
         (answ1 == "scissors" and answ2 == "rock"):
             score2 += 1
-            circle += 1
+            round += 1
             print("második játékos nyerte a kört.")
-    elif (answ1 == "paper" and answ2 == "rock") or \
-            (answ1 == "scissors" and answ2 == "paper") or \
-            (answ1 == "rock" and answ2 == "scissors"):
+    else:
             score1 += 1
-            circle += 1
+            round += 1
             print("első játékos nyerte a kört.")
 
 print("Vége a játéknak, eredmények:")

e935993b2f7b7f770ccfbe3e64b5184a90076c88
mate-byte <kissmate1213@freemail.hu>
2025-10-26 12:52:43 +0100

    Update flat_finder.py

--- homeworks/kissmate/hw_03_conditionals_loops/flat_finder.py
+++ homeworks/kissmate/hw_03_conditionals_loops/flat_finder.py
@@ -4,9 +4,9 @@ if any(city.strip() in [ "Washington"] for city in cities):
 if any(city.strip() in [ "Chicago"] for city in cities):
     print(f"Sarah definitly like that place.")
 budget = float(input("Enter your budget: "))
-if any(city.strip() in ["New York", "San Francisco"] for city in cities) and budget >= 4000:
+if any(city.strip() in ["New York", "San Francisco"] for city in cities) and budget <= 4000:
     print(f"Sarah will be happy to enter this citytis for that price.")
-if budget >= 3000:
+if budget <= 3000:
     print(f"Sarah good with this choice.")
 
 

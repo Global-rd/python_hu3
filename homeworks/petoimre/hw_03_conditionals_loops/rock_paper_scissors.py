8cb4ce67d74b92822da50fb9f4866e12e169d0ed
imiii668-create <imiii668@gmail.com>
2025-10-26 11:37:53 +0100

    good_working_but

---
+++ homeworks/petoimre/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -0,0 +1,43 @@
+# rock_paper_scissors
+
+while True:
+    max_rounds = int(input("Type in how many rounds do you want to play? "))
+    if (max_rounds % 2 != 0) and (max_rounds > 2):
+        break
+    else:
+        print("You need to enter an odd number which is more than one")
+
+
+gamer_A_point = 0
+gamer_B_point = 0
+act_round = 1
+while act_round <= max_rounds:
+    print(f"All rounds: {max_rounds}. Comming: {act_round}. Gamer_A: {gamer_A_point}, Gamer_B: {gamer_B_point}")
+    while True:
+        gamer_A_answer = input("GAMER_A > Type in your answer: ").lower()
+        if gamer_A_answer in ["rock", "paper", "scissors"]:
+            break
+        else:
+            print("Please type in the words according to rule!")
+    while True:
+        gamer_B_answer = input("GAMER_B > Type in your answer: ").lower()
+        if gamer_B_answer in ["rock", "paper", "scissors"]:
+            if gamer_A_answer == gamer_B_answer:
+                print("Same answer. Round again!")
+                break
+            elif (gamer_A_answer == "rock" and gamer_B_answer == "scissors") or (gamer_A_answer == "paper" and gamer_B_answer == "rock") or (gamer_A_answer == "scissors" and gamer_B_answer == "paper"):
+                act_round += 1
+                gamer_A_point += 1
+                break
+            else:
+                act_round += 1
+                gamer_B_point += 1    
+                break
+        else:
+            print("Please type in the words according to rule!")
+
+print(f"Game over. Result: GAMER_A: {gamer_A_point} point;  GAMER_B: {gamer_B_point} point. The winner: {"GAMER_A" if gamer_A_point > gamer_B_point else "GAMER_B"}")
+
+
+              
+  

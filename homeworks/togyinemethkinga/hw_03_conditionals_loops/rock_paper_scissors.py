fe2bb2878b51e2c8b1db971a1d0c3a3f9d4c7a40
tnkinga <nemeth.kingaa@gmail.com>
2025-10-26 13:23:28 +0100

    javitas

--- homeworks/togyinemethkinga/hw_03_conditionals_loops/rock_paper_scissors.py
+++ homeworks/togyinemethkinga/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -1,4 +1,4 @@
-game=["rock","paper","sciccors"]
+game=["rock","paper","scissors"]
 first_player_points = 0
 second_player_points = 0
 
@@ -11,22 +11,24 @@ while True:
         print("Please give me an odd number.")
 
  # Megvizsgáljuk, hogy melyik kört ki nyeri.
-round = 1
-while round <= rounds:
+current_round = 1
+while current_round <= rounds:
     first_player = input("What did the first player show?").strip().lower()
     second_player = input("What did the second player show?").strip().lower()
     if first_player in game and second_player in game: # Először érvényes értéket kell, hogy megadjon.
         if first_player != second_player: # Meg kell vizsgálni, hogy a körnek legyen győztese, tehát nem mutathatják ugyanazt.
-            if first_player == "rock" and second_player == "sciccors" or first_player == "sciccors" and second_player == "paper" or first_player == "paper" and second_player == "rock":
+            if ((first_player == "rock" and second_player == "scissors") or 
+            (first_player == "scissors" and second_player == "paper") or 
+            (first_player == "paper" and second_player == "rock")):
                 first_player_points += 1
             else: second_player_points += 1
-            round += 1
+            current_round += 1
         else: 
             print("Egal, you need to repeat.")
     else:
-        print("Please give me 'rock', 'paper' or 'sciccors'.")
+        print("Please give me 'rock', 'paper' or 'scissors'.")
 
-# Megvizsgálom, hogy ki nyert és annak mennyi pontja lett.
+# Megvizsgálom, hogy ki nyert és mennyi pontja lett.
 winner = "first player" if first_player_points > second_player_points else "second player"
 higher_point = first_player_points if first_player_points > second_player_points else second_player_points
 

f6a1ede3a00385322ddc1aba5d79e6470ed74309
imiii668-create <imiii668@gmail.com>
2025-10-26 18:27:59 +0100

    Homework-3-repaired

--- homeworks/petoimre/hw_03_conditionals_loops/rock_paper_scissors.py
+++ homeworks/petoimre/hw_03_conditionals_loops/rock_paper_scissors.py
@@ -1,11 +1,10 @@
 # rock_paper_scissors
 
 while True:
-    max_rounds = int(input("Type in how many rounds do you want to play? "))                # egy kör is kör
-    if (max_rounds % 2 != 0):                          #and (max_rounds > 1):
+    max_rounds = int(input("Type in how many rounds do you want to play? "))               
+    if (max_rounds % 2 != 0):                 
         break
-    else:
-        print("You need to enter an odd number")       #which is more than one")
+    print("You need to enter an odd number") 
 
 
 gamer_A_point = 0
@@ -18,15 +17,16 @@ while act_round <= max_rounds:
         gamer_A_answer = input("GAMER_A > Type in your answer (rock-paper-scissors): ").lower()
         if gamer_A_answer in ["rock", "paper", "scissors"]:
             break
-        else:
-            print("Please type in the word according to rule!")
+        print("Please type in the word according to rule!")
     while True:
         gamer_B_answer = input("GAMER_B > Type in your answer (rock-paper-scissors): ").lower()
         if gamer_B_answer in ["rock", "paper", "scissors"]:
             if gamer_A_answer == gamer_B_answer:
                 print("Same answer. Round again!")                           # nem növeli az act_round-ot
                 break                                                        # csak kiszáll
-            elif (gamer_A_answer == "rock" and gamer_B_answer == "scissors") or (gamer_A_answer == "paper" and gamer_B_answer == "rock") or (gamer_A_answer == "scissors" and gamer_B_answer == "paper"):
+            elif (gamer_A_answer == "rock" and gamer_B_answer == "scissors") or \
+                 (gamer_A_answer == "paper" and gamer_B_answer == "rock") or \
+                 (gamer_A_answer == "scissors" and gamer_B_answer == "paper"):
                 act_round += 1
                 gamer_A_point += 1
                 break

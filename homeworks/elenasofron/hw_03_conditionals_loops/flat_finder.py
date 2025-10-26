c86eedf571a49c00a10de34bbfd737015ff8330e
elenasofron <elena.sofronova@gmail.com>
2025-10-26 20:35:07 +0100

    Create flat_finder.py
    HW 3, 1 feladat

---
+++ homeworks/elenasofron/hw_03_conditionals_loops/flat_finder.py
@@ -0,0 +1,17 @@
+location = input ("Please write the city: ")
+price = int(input("Please write your price: "))
+
+if location == "Washington" and price > 0:
+    print("No way, you hate this city and you know it.")
+
+elif location == ("New York" or "San Francisco") and price < 4000:
+        print(f"Welcome to {location} and you monthly fee will be {price} USD")
+
+elif location == "Chicago":
+        print("You are welcome to Chicago baby, definitely you can afford it.")
+
+elif price < 3000:
+        print (f"Welcome to {location} and you monthly fee will be {price} USD")
+
+else:
+    print(f"No flat available with your parameters. Good bye")

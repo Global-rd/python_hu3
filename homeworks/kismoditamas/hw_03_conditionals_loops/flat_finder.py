2b4265151c64241f5db0c68a60afac4cb43ee2e0
tamaskismodi <kismodit@projectcloud.hu>
2025-10-27 19:55:13 +0100

    Homework3 ready
    ok

---
+++ homeworks/kismoditamas/hw_03_conditionals_loops/flat_finder.py
@@ -0,0 +1,17 @@
+place_name = input("Add meg a helységnevet: ").strip()
+cost =int( input("Add meg az árat USD-ben: "))
+
+if place_name.lower() == "washington":
+    print("Semmi pénzért nem lakna Washington városban.")
+elif cost >= 4000:
+    if place_name.lower() == "chicago":
+        print("Bármit megadna azért hogy Chicago városban lakhasson.")
+    else:
+        print("Ekkora összegért csak Chicago-t választaná.")
+else:
+    if place_name.lower() == "new york" or place_name.lower() == "san fransisco":
+        print(f"Kivenné a lakást { place_name.capitalize() } városban.")
+    elif cost < 3000:
+        print(f"Kivenné a lakást { place_name.capitalize() } városban.")
+    else:
+        print("Ekkora összegért csak New York-ot vagy San Fransisco-t választaná.")

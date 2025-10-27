d583aa7f229c89dc9f802bb8ac05b1b53496e88e
InnoIldi <Papp.Ildiko@innomatrix.hu>
2025-10-27 08:11:32 +0100

    Update flat_finder.py

--- homeworks/pappildiko/hw_03_conditionals_loops/flat_finder.py
+++ homeworks/pappildiko/hw_03_conditionals_loops/flat_finder.py
@@ -22,9 +22,9 @@ while True:
 
 if city in ["New York", "San Francisco"] and rental_price < 4000 :
     print(f"Sarah would like to rent the appartement in {city} for $ {rental_price:.2f}.")
-elif city in ["Chicago"]:
+elif city == "Chicago":
     print(f"Sarah would like to rent the appartement in {city} for $ {rental_price:.2f}.")
-elif city in ["Washington"]:
+elif city == "Washington":
     print(f"Sarah would not like to rent the appartement in {city} for $ {rental_price:.2f}.")
 elif rental_price <= 3000:
     print(f"Sarah would like to rent the appartement in {city} for $ {rental_price:.2f}.")

35ca022001b50df4b8b7485d17bdd0756463bf14
leventegellert <56770275+leventegellert@users.noreply.github.com>
2025-10-27 15:27:54 +0100

    homework_2_corrected

---
+++ homeworks/gellertlevi/homework_02_vars_data_types/user_info.py
@@ -0,0 +1,24 @@
+user_info = { "name": "Mike", "age": 25, "favourite_meals": [ "pizza", "carbonara", "sushi" ], "phone_contacts": { "Mary": "+36701234567", "Tim": "+36207654321", "Tim2": "+36304567321", "Jim": "+364005000" } }
+user_input = input("Specify four programming languages separated by commas: ")
+
+"""prog_lang1,prog_lang2, prog_lang3, prog_lang4= [x.strip() for x in user_input.split(",")]
+prog_lang_list = [prog_lang1,prog_lang2, prog_lang3, prog_lang4]"""
+user_info["skills"] = [x.strip() for x in user_input.split(",")]
+
+
+user_info["favourite_meals"].sort()
+
+print(user_info["favourite_meals"][-2])
+user_info["favourite_meals"].append("spaghetti")
+
+user_info["favourite_meals"].extend(user_info["favourite_meals"][2:4])
+user_info["favourite_meals"] = list(dict.fromkeys(user_info["favourite_meals"]))
+user_info["favourite_meals"] [0], user_info["favourite_meals"] [-1] = user_info["favourite_meals"] [-1], user_info["favourite_meals"] [0]
+
+user_info["phone_contacts"].update({"emergency_call": "112"})
+user_info["phone_contacts"].pop("Tim")
+
+print (user_info)
+
+
+

dd1dd72be5dbe387269c36afb482d2faa66baec9
tamaskismodi <kismodit@projectcloud.hu>
2025-10-28 21:12:15 +0100

    Update flat_finder.py

--- homeworks/kismoditamas/hw_03_conditionals_loops/flat_finder.py
+++ homeworks/kismoditamas/hw_03_conditionals_loops/flat_finder.py
@@ -1,7 +1,18 @@
-place_name = input("Add meg a helységnevet: ").strip()
+place_name = input("Add meg a helységnevet: ").strip().lower()
 cost =int( input("Add meg az árat USD-ben: "))
 
-if place_name.lower() == "washington":
+if place_name == "washington":
+    print("Semmi pénzért nem lakna Washington városban.")
+elif place_name == "chicago":
+    print("Bármit megadna azért hogy Chicago városban lakhasson.")
+elif place_name in ["san francisco", "new york"] and cost < 4000:
+    print(f"Kivenné a lakást { place_name.capitalize() } városban.")
+elif cost < 3000: #ez már csak olyan eset lehet ahol a városnév idegen, ezért elég a cost-ot nézni
+    print(f"Kivenné a lakást { place_name.capitalize() } városban.")
+else: #ez pedig csak az az eset lehet ahol a városnév idegen, és túl drága
+    print(f"Nem venné ki a lakást { place_name.capitalize() } városban.")
+
+""" if place_name.lower() == "washington":
     print("Semmi pénzért nem lakna Washington városban.")
 elif cost >= 4000:
     if place_name.lower() == "chicago":
@@ -14,4 +25,4 @@ else:
     elif cost < 3000:
         print(f"Kivenné a lakást { place_name.capitalize() } városban.")
     else:
-        print("Ekkora összegért csak New York-ot vagy San Fransisco-t választaná.")
+        print("Ekkora összegért csak New York-ot vagy San Fransisco-t választaná.")""" 

0ea80708378e39b764f9c10c85218839c8a68934
HorvathNorbert-dev <horvathnorbi@gmail.com>
2025-10-30 18:42:36 +0100

    Update flat_finder.py

--- homeworks/horvathnorbert/hw_03_conditionals_loops/flat_finder.py
+++ homeworks/horvathnorbert/hw_03_conditionals_loops/flat_finder.py
@@ -3,14 +3,12 @@ import os
 # Képernyő törlés.
 os.system('cls' if os.name == 'nt' else 'clear')
 
-# Kritériumok eltárolása egy mátrixban.
+# Kritériumok eltárolása egy dictionaryben.
 
-flat_criteria = [
-    ["newyork","sanfransisco",4000],
-    ["chicago"],
-    ["washington"],
-    [3000]
-]
+flat_criteria = {"newyork": 4000,
+                "sanfrancisco": 4000,
+                "chicago": float('inf'),
+                "washington": float('-inf')}
 
 city_name = input("Name of the city? : ").lower().replace(" ","") #Szóközök eltávolítása és kisbetűsként való eltárolás.
 
@@ -18,16 +16,19 @@ flat_price = int(input("Flat price? : ").replace(" ","")) #Szóközök eltávol�
 
 # A feladatban leírt kritériumok ellnőrzés.
 
-if (city_name == flat_criteria[0][0] or city_name == flat_criteria[0][1]) and flat_price < flat_criteria[0][2]:
+if city_name == "newyork" and flat_price < flat_criteria[city_name]:
     print("I would love to rent this apartment.")
 
-elif (city_name == flat_criteria[1][0]):
+elif city_name == "sanfrancisco" and flat_price < flat_criteria[city_name]:
+    print("I would love to rent this apartment.")
+
+elif city_name == "chicago":
     print("I love Chicago! It is worth every cent to live here.")
 
-elif (city_name == flat_criteria[2][0]):
+elif city_name == "washington":
     print("I hate Washington! You could not pay me to live here!")
 
-elif (flat_price < flat_criteria[3][0]):
+elif flat_price < 3000:
     print("I would love to rent this apartment.")
 else:
     print("I would not like to rent this apartment. Maybe if it were a bit cheaper.")

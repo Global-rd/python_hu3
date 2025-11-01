0bd050e086d8e0e4ccca2a9a284a77a47f76d972
osandor77 <osandor77@gmail.com>
2025-11-01 17:15:57 +0100

    OlahSandor/hw 4 done

--- homeworks/olahsandor/hw_04_exceptions_logging/to_do.py
+++ homeworks/olahsandor/hw_04_exceptions_logging/to_do.py
@@ -1,30 +1,40 @@
-import logging
 import os
+import logging
+from pathlib import Path
+from logging_config import setup_logging
+#ellenőrizve a cwd
+#print(os.getcwd())
 
-#Fájlok elérési útjai - a mappába tettem őket, ahol a to_do.py is van
-tasks_file = "task.txt"
-log_file = "app.log" 
+# csere akkor a pathlib-re
+tasks_file = Path("homeworks") / "olahsandor" / "hw_04_exceptions"/"task.txt" #relative path
+#akkor ez nem kell
+#log_file = Path("homeworks") / "olahsandor" / "hw_04_exceptions"/"app.log" #relative path
+
+#tasks_file = " homeworks/olahsandor/hw_04_exceptions/task.txt"
+#log_file = " homeworks/olahsandor/hw_04_exceptions/app.log"
 
 # Logolás, az óráról
-file_handler = logging.FileHandler(log_file)
-stream_handler = logging.StreamHandler()
-formatter_file = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-formatter_cons = logging.Formatter('%(message)s')
-file_handler.setFormatter(formatter_file)
-stream_handler.setFormatter(formatter_cons)
-file_handler.setLevel(logging.ERROR)
-stream_handler.setLevel(logging.INFO)
-logger = logging.getLogger()
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
-logger.setLevel(logging.DEBUG)
-
+#file_handler = logging.FileHandler(log_file)
+#stream_handler = logging.StreamHandler()
+#formatter_file = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+#formatter_cons = logging.Formatter('%(message)s')
+#file_handler.setFormatter(formatter_file)
+#stream_handler.setFormatter(formatter_cons)
+#file_handler.setLevel(logging.ERROR)
+#stream_handler.setLevel(logging.INFO)
+#logger = logging.getLogger()
+#logger.addHandler(file_handler)
+#logger.addHandler(stream_handler)
+#logger.setLevel(logging.DEBUG)
+setup_logging()
+logger = logging.getLogger(__name__)
 
 #Feladatok olvasása:
-def load_tasks_from_file():
+#def load_tasks_from_file():
+def load_tasks_from_file(logger):
     try:
         with open(tasks_file, "r") as file:
-# Beolvassuk a sorokat, eltávolítjuk a whitespace-t
+            # Beolvassuk a sorokat, eltávolítjuk a whitespace-t
             tasks_list = []
             lines = file.readlines()
             for line in lines:
@@ -35,17 +45,17 @@ def load_tasks_from_file():
         return tasks_list
     except FileNotFoundError:
         logger.error(f"Task file '{tasks_file}' not found. Starting with an empty list.")
-# Üres lista visszaadása
+        # Üres lista visszaadása
         return [] 
     except Exception as e:
         logger.error(f"An unexpected error occurred during file loading: {e}")
         return []
 
 #Feladatok Írása:
-def save_tasks_to_file(tasks_list):
+def save_tasks_to_file(tasks_list,logger):
     try:
         with open(tasks_file, "w") as file:
-# Felülírjuk a fájlt a memóriábanmal
+            # Felülírjuk a fájlt a memóriában
             file.writelines(f"{task}\n" for task in tasks_list)
         logger.info(f"Tasks saved successfully: {len(tasks_list)} items.")
     except Exception as e:
@@ -59,34 +69,34 @@ def view_tasks(tasks_list):
         for i, task in enumerate(tasks_list, 1):
             print(f"{i}. {task}")
     else:
-        logger.info("The task list is empty.")
+        print("The task list is empty.")
     
-# Mivel ez a függvény nem módosítja a listát, az eredeti listával tér vissza
+    # Mivel ez a függvény nem módosítja a listát, az eredeti listával tér vissza
     return tasks_list
 
 
 #Feladat Hozzáadása
-def add_task(tasks_list, task_to_add):
+def add_task(tasks_list, task_to_add,logger):
     task = task_to_add.strip()
     if task:
         tasks_list.append(task)
-        print(f"'{task}' added to list (in memory).")
+        logger.info(f"'{task}' added to list (in memory).")
     else:
         logger.info("The task to add cannot be empty.")
-# A frissített lista visszaadása
+    # A frissített lista visszaadása
     return tasks_list 
 
 #Feladat Törlése
-def remove_task(tasks_list, task_to_remove):
+def remove_task(tasks_list, task_to_remove, logger):
     task = task_to_remove.strip()
 
     try:
         tasks_list.remove(task)
-        print(f"'{task}' deleted from memory.")
+        logger.info(f"'{task}' deleted from memory.")
     except ValueError:
         # Akkor dobódik, ha az elem nincs a listában
         logger.error(f"There is no such task: '{task}'!")
-# A frissített lista visszaadása
+    # A frissített lista visszaadása
     return tasks_list 
 
 
@@ -118,30 +128,32 @@ def display_menu():
 
 #fő Program
 def main():
-    
-# 1. A fájlból való olvasás automatikusan megtörténik a program indulásakor
-    tasks_list = load_tasks_from_file() 
+    #MÓDOSÍTÁS    
+    #Logolás a fájl alapján, ez új a logging_config alapján
     logger.info("Program RUN started.")
+    logger.debug("Debug info here")
+    tasks_list = load_tasks_from_file(logger)
+
 
     while True:
         chosen = display_menu() 
         
         if chosen == 1:
             task = input("What is the task to add?: ").strip()
-# A függvény frissíti a listát, és a visszatérési értékkel frissítjük a tasks_list-et
-            tasks_list = add_task(tasks_list, task) 
+            # A függvény frissíti a listát, és a visszatérési értékkel frissítjük a tasks_list-et
+            tasks_list = add_task(tasks_list, task, logger) 
         
         elif chosen == 2:
             tasks_list = view_tasks(tasks_list) 
         
         elif chosen == 3:
             task = input("What is the task to remove?: ").strip()
-# A függvény frissíti a listát, és a visszatérési értékkel frissítjük a lokális tasks_list-et
-            tasks_list = remove_task(tasks_list, task) 
+            # A függvény frissíti a listát, és a visszatérési értékkel frissítjük a lokális tasks_list-et
+            tasks_list = remove_task(tasks_list, task, logger) 
         
         elif chosen == 4:
-# 2. Fájlba írás csak a program befejezésekor
-            save_tasks_to_file(tasks_list) 
+            # 2. Fájlba írás csak a program befejezésekor
+            save_tasks_to_file(tasks_list, logger) 
             logger.info("Program RUN completed. Exiting.")
             break
 

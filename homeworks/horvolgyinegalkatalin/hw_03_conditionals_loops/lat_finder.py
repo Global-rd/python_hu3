2c7dc62f5acf13674ac405c76aafa20e651e31c0
kagal <kagal@freemail.hu>
2025-11-03 21:41:13 +0100

    harmadik házi első fele

---
+++ homeworks/horvolgyinegalkatalin/hw_03_conditionals_loops/lat_finder.py
@@ -0,0 +1,15 @@
+# feladat: város és bérleti díj bekérése
+# ha város=Chicago akkor költözik
+# ha a város =Washington akkor nem költözik
+# ha a város = New York vagy San Francisco és a bérleti díj max 4000 akkor költözik
+# ha a város bérmi más és a bérletidíj kisebbegyenlő mint 3000 akkor költözik
+# if-elif-else, operátorok
+
+city=input("Kérem adjon meg egy város nevét:").upper()
+rent=float(input("adja meg a bérleti díjat:"))
+
+if city==("CHICAGO"):print("költözik")
+elif city==("WASHINGTON"):print("nem költözik")
+elif city==("NEW YORK" or "SAN FRANCISCO") and (rent<=4000):print("költözik")
+elif rent<=(3000):print("költözik")
+else : print("nem költözik")

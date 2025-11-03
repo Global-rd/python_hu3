194eecfdc4a5b8fab6f92bd081230dd903d38072
torokati <torok.attila74@gmail.com>
2025-11-03 22:45:55 +0100

    hw04 Done
    Kész van, működik szerintem :) De érzek némi zavart még az erőben a főleg a hibakezelés területén. Várom az észrevételeidet!

--- homeworks/torokattila/hw_04_exceptions_logging/to_do.py
+++ homeworks/torokattila/hw_04_exceptions_logging/to_do.py
@@ -1,35 +1,87 @@
+import os
+from pathlib import Path
+
+from logging_config import setup_logging
+import logging
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
+file_path = Path("homeworks") / "torokattila" / "hw_04_exceptions_logging" / "tasks.txt"
+
 # Functions declaration
-def read_task():
-    pass
-def write_task():
-    pass
-def display_task(task_list):
+# Beolvassa a tasks.txt fájlból a TASK információkat.
+def read_tasks(file_path):
+    with open(file_path,"r",encoding="UTF-8") as file:
+        for line in file:
+            yield line.strip()
+    logger.info("Data read from file")
+# Kiírja a task.txt fájlba a TASK információkat a programból való kilépéskor
+def write_tasks(task_list):
+    try:
+        with open(file_path,"w",encoding="UTF-8") as file:
+            try:
+                for line in task_list:            
+                    file.write(line + "\n")  
+            except:
+                print ("Error writing file)")
+    except:
+        print("Error opening file")
+    logger.info("Data write into file - 2")
+def display_tasks(task_list):
+    print("-----------")
     print("Tasks list:")
+    print("-----------")
     for task in task_list:
         print(task)
     
-
 def add_task(task_list, name:str):
     task = name
     task_list.append(task)
+    logging.info("Task added")    
 
 def remove_task(task_list, name:str):
     for task in task_list:
         if task == name:
             task_list.remove(task)
+            logging.info("Task deleted")
             return
-
+    
 def display_menu():
-    pass
+    print ("M E N U")
+    print("-----------")
+    print ("1 - Add a new task")
+    print ("2 - View tasks")
+    print ("3 - Remove a task")
+    print ("4 - Exit")
 
 def main():
+    logger.info("Application started")
     task_list = []
-    add_task(task_list, "Ebéd")
-    add_task(task_list, "Uzsi")
-    add_task(task_list, "Vacsora")
-    #add_task(task_list, "Ebéd")
-    remove_task(task_list, "Ebéd")
+    # Ha létezik a tasks.txt akkor beolvassuk soronként és beírjuk a task_list-be.        
+    if file_path.exists():
+        for line in read_tasks(file_path):
+            task_list.append(line.strip())
     
-    display_task(task_list)
+    while True:
+        display_menu()
+        try:
+            answare = int(input ("Choose an option: "))
+            
+            if answare == 4:
+                write_tasks(task_list)
+                break
+            if answare == 3:
+                task_name_to_delete = input("Which task should I delete? ")
+                remove_task(task_list,task_name_to_delete)
+            if answare == 2:
+                display_tasks(task_list)
+            if answare == 1:
+                task_name_to_add = input("What is name the new task ? ")
+                add_task(task_list,task_name_to_add)
+                
+        except ValueError as e:            
+            logger.exception(f"An error occured: {e}")                        
+    logger.info("Application quit")
 
 main()

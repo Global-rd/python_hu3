83f8df5866a231df129b3f7528bf3d8995f077c7
kagal <kagal@freemail.hu>
2025-11-15 21:22:42 +0100

    kész a kért javítás

--- homeworks/horvolgyinegalkatalin/hw_04_exceptions_logging/to_do.py
+++ homeworks/horvolgyinegalkatalin/hw_04_exceptions_logging/to_do.py
@@ -43,17 +43,20 @@ LOGGING_CONFIG = {
 
 def setup_logging():
     logging.config.dictConfig(LOGGING_CONFIG)
-
-def  read_file(to_do_txt_path:Path):
-    strip_lines=[]
-    with open(to_do_txt_path, "r") as file:
-        lines = file.readlines()
-        print(lines)
-    
-        for line in lines:
-            row=line.strip()
-            strip_lines.append(row)
-            print(row)
+def read_file(to_do_txt_path: Path):
+    strip_lines = []
+    try:
+        with open(to_do_txt_path, "r", encoding="utf-8") as file:
+            lines = file.readlines()
+            for line in lines:
+                strip_lines.append(line.strip())
+    except FileNotFoundError:
+        logger.error(f"A feladatfájl nem található: {to_do_txt_path}")
+        # új üres fájlt hozunk létre
+        to_do_txt_path.touch()
+    except Exception as e:
+        logger.exception("Váratlan hiba a fájl olvasása közben.")
+        raise
     return strip_lines
 
 def write_file(to_do_txt_path:Path,write_list:list): #feltételezem, hogy ugyanazzal a fileal dolgozunk és nem egy másik értékkel
@@ -83,42 +86,48 @@ logger=logging.getLogger(__name__)
 
 menu=["1. Add Task:","2.View Tasks:", "3. Remove Tasks:", "4. Exit"]
 
-file_path = Path("homeworks") / "horvolgyinegalkatalin" / "hw_04_exceptions_logging" / "to_do.txt" #relative path
+def main():
 
-task_list=read_file(file_path)
-logger.info("beolvastam az eddigi feladatokat a fájlból")
-display_menu(menu)
+    file_path = Path("homeworks") / "horvolgyinegalkatalin" / "hw_04_exceptions_logging" / "to_do.txt" #relative path
 
-while True:
-    try:
-        menu_mumber=int(input("Válassz a menüpontok közül 1-4-ig:"))
+    task_list=read_file(file_path)
+    logger.info("beolvastam az eddigi feladatokat a fájlból")
+    display_menu(menu)
 
-    except ValueError:
-        logger.error("Csak számot adhatsz meg!")
-        continue    
-    if 1<= menu_mumber <=4:
-            
-        if menu_mumber==1:
-            logger.debug("hozzáadom az ój feladatot a listához")
-            task=input("Add meg a feladatot:")
-            add_tasks(task_list,task)
+    while True:
+        try:
+            menu_mumber=int(input("Válassz a menüpontok közül 1-4-ig:"))
+
+        except ValueError:
+            logger.error("Csak számot adhatsz meg!")
+            continue    
+        if 1<= menu_mumber <=4:
                 
-        elif menu_mumber==2:
-            logger.debug("most kiírom a listát a képernyőre")
-            view_tasks(task_list)
-    
-        elif menu_mumber==3:
-            task=input("Add meg a feladatot:")
-            del_task(task_list,task)
-            logger.debug("kitörlődött a törölni való")
-    
-        elif menu_mumber==4:
-            logger.debug("Most fogom a fájlba kiírni visszaírni a to_do listát az 4 menüpont választása miatt")
-            write_file(file_path,task_list)
-            break
+            if menu_mumber==1:
+                logger.debug("hozzáadom az ój feladatot a listához")
+                task=input("Add meg a feladatot:")
+                add_tasks(task_list,task)
+                    
+            elif menu_mumber==2:
+                logger.debug("most kiírom a listát a képernyőre")
+                view_tasks(task_list)
 
-    else:
-        logger.warning("csak 1-4-ig adhatsz meg számokat!")
+            elif menu_mumber==3:
+                task=input("Add meg a feladatot:")
+                del_task(task_list,task)
+                logger.debug("kitörlődött a törölni való")
+
+            elif menu_mumber==4:
+                logger.debug("Most fogom a fájlba kiírni visszaírni a to_do listát az 4 menüpont választása miatt")
+                write_file(file_path,task_list)
+                break
+
+        else:
+            logger.warning("csak 1-4-ig adhatsz meg számokat!")
     
 
+if __name__ == "__main__":
+    setup_logging()
+    logger = logging.getLogger(__name__)
+    main()
 

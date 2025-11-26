a8d00d093504314e6d0ea71438fba1689ff0ef85
ab-sem <balazs.agocs@sempergy.hu>
2025-11-26 10:26:56 +0100

    agocsbalazs/hw-6
    agocsbalazs/hw-6

---
+++ homeworks/agocsbalazs/hw_06_testing/test_bank_account.py
@@ -0,0 +1,57 @@
+#testelő py file Mesefigurák banki számlájához
+
+from bank_account import BankAccount
+import pytest
+
+
+@pytest.fixture
+def account():
+    return BankAccount("Tom&Jerry", 100.0)
+
+@pytest.fixture
+def account_2():
+    return BankAccount("Captain_Balu", 50.0)
+
+@pytest.mark.parametrize("amount", [-50.0, 1000.0])
+def test_deposit_invalid_values(account: BankAccount, amount):
+    with pytest.raises(ValueError):
+        account.deposit(amount)
+
+#-----------------------------
+
+def test_deposit_negative_amount():
+    account = BankAccount("Tom&Jerry", 100)
+    with pytest.raises(ValueError):
+        account.deposit(-50)
+
+def test_deposit_non_number():
+    account_2 = BankAccount("Captain_Balu", 100)
+    with pytest.raises(TypeError):
+        account_2.deposit("fifty")
+
+def test_withdraw_negative_amount():
+    account = BankAccount("Tom&Jerry", 100)
+    with pytest.raises(ValueError):
+        account.withdraw(-30)
+
+def test_withdraw_non_number():
+    account_2 = BankAccount("Captain_Balu", 100)
+    with pytest.raises(TypeError):
+        account_2.withdraw("fifty")
+
+def test_withdraw_insufficient_funds():
+    account = BankAccount("Tom&Jerry", 100)
+    with pytest.raises(ValueError):
+        account.withdraw(150)
+
+def test_transfer_to_self():
+    account = BankAccount("Tom&Jerry", 100)
+    with pytest.raises(ValueError):
+        account.transfer(50, account)
+
+        
+def test_transfer_insufficient_funds():
+    account_1 = BankAccount("Tom&Jerry", 100)
+    account_2 = BankAccount("Captain_Balu", 100)
+    with pytest.raises(ValueError):
+        account_1.transfer(150, account_2)

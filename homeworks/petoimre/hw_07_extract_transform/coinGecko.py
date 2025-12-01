ae9c9f9c7320d6ea7509126cd51ef708adb72880
imiii668-create <imiii668@gmail.com>
2025-12-01 22:23:29 +0100

    Homework-is-ready

--- homeworks/petoimre/hw_07_extract_transform/coinGecko.py
+++ homeworks/petoimre/hw_07_extract_transform/coinGecko.py
@@ -1,5 +1,6 @@
 import requests
 import settings as s
+import pandas as pd
 
 
 # basic_url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false"
@@ -15,12 +16,27 @@ response = requests.get(url=basic_url, params=api_params).json()
 
 
 
-#print(response.status_code)
-#print(response.content)
-#print(response.text)
-#print(type(response.text))
-print(response)                        # json esetén
-print(type(response))
+
+
+df = pd.DataFrame(response)
+print(df)
+print(df.info())                                   # 1./
+
+sum_market_cap = df["market_cap"].sum()            # 2./
+print(sum_market_cap)
+
+top50_df = df.nlargest(50, "current_price")        # 3./
+print(top50_df)
+
+desc_by_pcp24h = top50_df.sort_values("price_change_percentage_24h", ascending=False)    # 4./
+print(desc_by_pcp24h)
+
+top50_df["change_direction"] = "0"            
+top50_df.loc[df["price_change_percentage_24h"] > 0, "change_direction"] = "+"           # 5./
+top50_df.loc[df["price_change_percentage_24h"] < 0, "change_direction"] = "-"
+
+print(top50_df)
+
 
 
 

61eabc8ac927c4b72c94f658e043dd4fcaeb73bb
imiii668-create <imiii668@gmail.com>
2025-12-01 22:27:01 +0100

    wo_API_KEY

--- homeworks/petoimre/hw_07_extract_transform/settings.py
+++ homeworks/petoimre/hw_07_extract_transform/settings.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-COINGECKO_API_KEY = os.environ.get("COINGECKO_API_KEY")
+#COINGECKO_API_KEY = os.environ.get("COINGECKO_API_KEY")
 CURRENCY = "usd"
 ORDER = "market_cap_desc"
 PER_PAGE = "250"

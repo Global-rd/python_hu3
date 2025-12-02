4f9231fc7362feb6d0813122dd4cabc584c248fe
kagal <kagal@freemail.hu>
2025-12-02 21:31:41 +0100

    7-es házi javítás kész

--- homeworks/horvolgyinegalkatalin/hw_07_extract_transform/CG_adatelemzés.py
+++ homeworks/horvolgyinegalkatalin/hw_07_extract_transform/CG_adatelemzés.py
@@ -5,7 +5,7 @@ url = "https://api.coingecko.com/api/v3/coins/markets"
 params = {"vs_currency": "usd", "per_page": 250}
 
 response = requests.get(url, params=params)
-data = response.json = ()
+data = response.json()
 
 df = pd.DataFrame(data)
 

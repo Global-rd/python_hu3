3735f18781862d8c740c13eb4c6626745cb677c2
imiii668-create <imiii668@gmail.com>
2025-12-14 16:32:47 +0100

    petoimre_HW_10

---
+++ homeworks/petoimre/hw_10_web_scraping/quotes_selectors.py
@@ -0,0 +1,8 @@
+
+
+
+TOP_TAGS_XPATH = '//h2[text()="Top Ten tags"]/following-sibling::span[@class="tag-item"]/a'
+QUOTE_BLOCK_XPATH = '//div[@class="quote"]'
+QUOTE_TEXT_XPATH = './/span[@class="text"]'
+AUTHOR_XPATH = './/small[@class="author"]'
+NEXT_BUTTON_XPATH = '//li[@class="next"]/a'

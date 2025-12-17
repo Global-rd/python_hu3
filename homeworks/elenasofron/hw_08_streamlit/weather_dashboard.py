db4d606a3141137513962fa2f659998ed5430875
elenasofron <elena.sofronova@gmail.com>
2025-12-17 21:45:28 +0100

    Update weather_dashboard.py

--- homeworks/elenasofron/hw_08_streamlit/weather_dashboard.py
+++ homeworks/elenasofron/hw_08_streamlit/weather_dashboard.py
@@ -1,50 +1,45 @@
 import streamlit as st
 import requests
 import pandas as pd
-import plotly.express as px
 
 #api_key_weather = ["b2c2a2a969cb237ff0df24a3eb6b374f"]
 
-print(st.secrets["openweather"]["api_key_weather"])
+API_KEY = st.secrets["openweather"]["api_key_weather"]
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
+#print(st.secrets["openweather"]["api_key_weather"])
 
+#@st.cache_data(ttl=600)
 
-'''
-@st.cache_data(ttl=600)
-
-def get_current_weather(city, api_key_weather):
-    url = "https://api.openweathermap.org/data/2.5/weather"
-
-    params = {
-        "q": city,
-        "appid": api_key_weather,
-        "units": "metric",
-        "lang": "hu"
-    }
-
-    response = requests.get(url, params=params)
+def get_current_weather(city):
+    url = f"{BASE_URL}/?q={city}&appid={API_KEY}&units=metric&lang=en"
+    response = requests.get(url)
 
     if response.status_code != 200:
         return None
     return response.json()
+
 
 st.title("WEATHER DASHBOARD")
 
 city = st.text_input("Write a city:")
 
 if city:
-    api_key_weather = "b2c2a2a969cb237ff0df24a3eb6b374f"
+    current_weather = get_current_weather(city)
     
-    # Current weather
-    data = get_current_weather(city, api_key_weather)
+    if current_weather:
+        city = current_weather['name']
+        st.header(f"Current Weather in {city}")
 
-    if data:
-        st.subheader(f"Current city: **{city}**")
-        st.write(f"Current temperature: {data['main']['temp']} °C")
+        c1, c2, c3 = st.columns(3)
 
-        col.metric("Temperature (°C)", f"{data['main']['temp']} °C")
+        temp = current_weather['main']['temp']
+        humidity = current_weather['main']['humidity']
+        wind_speed = current_weather['wind']['speed']
+
+        c1.metric("Temperature (°C)", f"{temp}°C")
+        c2.metric("Humidity (%)", f"{humidity}%")
+        c3.metric("Wind Speed (m/s)", f"{wind_speed} m/s")
 
     else:
         st.error("ERROR")
-
-'''
